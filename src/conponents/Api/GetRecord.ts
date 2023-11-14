ac61eaa0b5c6cd913d5cc5bab94d14e68cdae2d9
kaniery <133332961+kaniery@users.noreply.github.com>
2023-11-15 00:41:38 +0900

    test

--- src/conponents/Api/GetRecord.ts
+++ src/conponents/Api/GetRecord.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const GetRecord = () => {
-  const url = "";
+  const url = "http://localhost:8080/csrf";
   axios
     .get(url)
     .then(function (response) {
--- src/conponents/Api/PostRecord.ts
+++ src/conponents/Api/PostRecord.ts
@@ -1,22 +1,29 @@
 import axios from "axios";
-import { useContext } from "react";
-import { UserIdContext } from "../../provider/ContextProviders";
+//import { useContext } from "react";
+//import { UserIdContext } from "../../provider/ContextProviders";
 
-export const PostApi = (/*Props*/) => {
-  const url = "";
-  const [id] = useContext(UserIdContext);
+export const PostRecord = (/*Props*/) => {
+  const url = "http://localhost:8080/signup";
+  //const [id] = useContext(UserIdContext);
 
   const postData = {
-    id: `${id}`,
+    //id: `${id}`,
+    email: "cccc@bbbb.com",
+    password: "password",
     /*date: `${date}`,
     start_at: `${startAt}`,
     end_at: `${endAt}`,
     hours: `${hours}`,
     content: `${content}`,*/
+    /*date: "20231112",
+    start_at: "0821",
+    end_at: "2130",
+    hours: "1309",
+    content: "新年のあいさつ",*/
   };
 
   axios
-    .post(url, Object.assign({}, postData))
+    .post(url, Object.assign({}, postData), { withCredentials: true })
     .then(function (response) {
       console.log(response);
     })
